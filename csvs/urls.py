d01ec13091c517fdbd8ecab3094fa03ae50196db
Michel Atieno <mishqamish@gmail.com>
2021-07-08 14:23:57 +0300

    Added UI slightly

--- csvs/urls.py
+++ csvs/urls.py
@@ -4,6 +4,6 @@ from .views import upload_file_view, variety_profile
 app_name='csvs'
 
 urlpatterns = [
-    path('', upload_file_view, name='upload-view'),
+    path('', upload_file_view, name="upload-view"),
     path('variety/<id>', variety_profile, name="variety_profile"),
 ]
