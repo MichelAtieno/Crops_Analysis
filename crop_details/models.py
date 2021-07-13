ea8d016b4039b4a0e3b1848d41a503f96ba75e32
Michel Atieno <mishqamish@gmail.com>
2021-07-13 10:45:42 +0300

    Able to upload the entire csv file, inclusive of entries with null values. Now user is able to view particular crop page.

--- crop_details/models.py
+++ crop_details/models.py
@@ -6,7 +6,7 @@ from django.shortcuts import reverse
 
 class Product_Variety(models.Model):
     variety = models.CharField(max_length=200)
-    image = models.ImageField(blank="True", null="True")
+    image = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return self.variety
@@ -29,21 +29,32 @@ UNITS = (
 )
 
 
-class Commodity_Type(models.Model):
+class Crops(models.Model):
     commodity = models.CharField(max_length=100)
-    product_variety = models.ManyToManyField(Product_Variety)
+    product_variety = models.ManyToManyField(Product_Variety, related_name='produce_variety')
     image = models.ImageField(default='')
     unit = models.CharField(max_length=20, choices=UNITS)
     volume_in_kgs = models.PositiveIntegerField()
     values_in_ksh = models.FloatField(null=True, blank=True, default=0.0)
     date = models.TextField(blank=True, null=True)
+    slug = models.SlugField()
 
 
     def __str__(self):
-        return f"{self.commodity}"
+        return f"{self.commodity}-{self.slug}"
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
+    def get_absolute_url(self):
+        return reverse('csvs:crop', kwargs={
+            'slug': self.slug
+        })
 
+class Commodity_Type(models.Model):
+    commodity = models.ForeignKey(Crops, on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.commodity
    
